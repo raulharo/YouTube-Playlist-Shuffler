bd4a435fae5762c5cfe0ea0827c42ce2cd442bc7
Raul Haro <raulharo99@gmail.com>
2023-07-11 21:08:28 -0400

    Created React project for front end of app

--- YT-Playlist-Shuffler/src/main/java/org/example/Main.java
+++ YT-Playlist-Shuffler/src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
         System.out.println("Hello world!");
     }
 }
---
+++ YT-Playlist-Shuffler/src/main/java/org/example/model/Playlist.java
@@ -0,0 +1,10 @@
+package org.example.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Playlist {
+    int playList_length;
+
+}
